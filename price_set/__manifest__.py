fe2f90a7b5cf1fa4d99d9ee488082064342f6454
Randy <ciprianrandy@gmail.com>
2025-11-05 23:21:40 -0400

    new method to edit

--- price_set/__manifest__.py
+++ price_set/__manifest__.py
@@ -4,7 +4,6 @@
     "depends": ["sale","sale_management"],
     "data": [
         "views/price_list.xml",    
-        "views/sale_order_form.xml"
     ],
     "application":False,
     "auto_install":False,
--- price_set/models/sale_order.py
+++ price_set/models/sale_order.py
@@ -1,26 +1,26 @@
-from odoo import models , fields, api
+from odoo import models, fields, api
 
 class SaleCustom(models.Model):
     _inherit = "sale.order"
-    
-    view_btn_apply_change = fields.Boolean()
-        
-    @api.onchange("pricelist_id")
-    def _onchange_pricelist(self):
-        for record in self:
-            if record.pricelist_id:
-                if record.pricelist_id.margen > 0:
-                    record.view_btn_apply_change = True
-                
-                else:
-                    record.view_btn_apply_change = False
-    
-    
+
+    view_btn_apply_change = fields.Boolean(string="View Apply Change", default=False)
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        res = super().create(vals_list)
+        for record in res:
+            record.apply_margen()
+        return res
+
+    def write(self, vals):
+        res = super().write(vals)
+        self.apply_margen()
+        return res
+
     def apply_margen(self):
         for record in self:
-            margen = record.pricelist_id.margen
+            margen = record.pricelist_id.margen or 0.0
             for line in record.order_line:
-                without_itbis = (line.price_unit / 100) * margen +  line.price_unit
-                line.price_unit = without_itbis
-
-        self.view_btn_apply_change = False
+                base_price = line.product_id.list_price
+                line.price_unit = base_price * (1 + margen / 100)
+            record.view_btn_apply_change = False
